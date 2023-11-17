ea1b2ad4b7160d063709af4035b2875b63a099f7
Hanif <102041408+hanifnrh@users.noreply.github.com>
2023-11-17 18:48:17 +0700

    commit

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className='about'>
             <h1>About Us</h1>
-            <p>Hi I'm Nurrahmat Hanif and this is my Final Task for Praktikum PPB, I made on my own using the base code of Modul 6 Praktikum PPB. With RapidAPI, I am able to provide youtube videos in this progressive web app.</p>
+            <p className='description'>Hi I'm Nurrahmat Hanif and this is my Final Task for Praktikum PPB, I made on my own using the base code of Modul 6 Praktikum PPB. With RapidAPI, I am able to provide youtube videos in this progressive web app.</p>
 
             <img src='https://img-14.stickers.cloud/packs/d44c5b37-35ac-465d-8702-9249c13316f8/webp/54c799e9-d6e2-4770-99b5-45c124d29c93.webp' alt='MalasBanget'></img>
         </div>
